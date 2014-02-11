2827a05203433c3e1b37068f437ee2f5856d3afe
SbSpider <rajshorya@gmail.com>
2014-02-12 12:43:06 +1300

    Potential way to fix the problem with cross compatibility.
    The use of the #if statements could work

--- src/c/bitcloud.h
+++ src/c/bitcloud.h
@@ -1,6 +1,29 @@
-#include <stdlib.h>
-/* external libs includes: */
-#include <openssl/ssl.h>
+
+// Platform detection
+#define PLATFORM_WINDOWS
+#define PLATFORM_MAC
+#define PLATFORM_UNIX
+
+#if defined(_WIN32)
+#define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#define PLATFORM PLATFORM_MAC
+#else
+#define PLATFORM PLATFORM_UNIX
+#endif
+
+#if PLATFORM == PLATFORM_WINDOWS
+#pragma comment(lib, "wsock32.lib")
+    #include <winsock2.h>
+#elif PLATFORM == PLATFORM_MAC || PLATFORM == PLATFORM_UNIX
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <fcnt1.h>
+#endif
+
+
+/* External lib includes */
+
 #include <sqlite3.h>
 
 
