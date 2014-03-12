857582120ac01957b4ef81f4b016ca4595fbda37
JavierRSobrino <liberman@riseup.net>
2014-03-12 16:19:17 +0100

    bitcloud.h cleanup

--- src/bitcloud.h
+++ src/bitcloud.h
@@ -40,30 +40,6 @@ BCError bc_auth (void *user_data,
                  const char *db_name,
                  const char *trigger);
 
-typedef struct BCNode {
-  BCKey public_key;
-  BCKey signature;
-  BCTime creation_date;
-  BCKey proof_of_creation;
-  BCInteger net_protocol;
-  BCTime last_online;
-  BCSize storage_capacity;
-  BCInteger storage_reputation;
-  BCInteger bandwidth_reputation;
-  BCInteger service_reputation;
-  BCInteger availability;
-  char address[0]; /* null terminated */
-} BCNode;
-
-BCError bc_register_node (BCNode *node);
-BCError bc_get_node (BCNode *dest, BCKey public_key);
-BCError bc_update_node (BCNode *node);
-
-BCError bc_check_node_ca (BCNode *node);
-BCError bc_check_node_creation (BCNode *node);
-
-
-
 /*
   Connections structures and functions
 */
@@ -83,58 +59,3 @@ typedef struct BCConnection {
 extern BCConnection *bc_Connections;
 extern int n_Connections;
 
-BCError bc_node_connect (BCConnection *con, BCKey public_key);
-BCError bc_node_disconnect (BCConnection *con);
-
-
-/*
-  Filesystem structures
-*/
-
-typedef struct BCFolder {
-} BCFolder;
-
-
-/*
-  User structures and functions
-*/
-typedef struct BCUser {
-  BCKey public_key;
-  struct BCPublisher *publisher;
-  BCKey publisher_signature;
-  char *nickname;
-  BCTime revocation_date;
-  BCSize storage_quota;
-  BCSize bandwidth_quota;
-  BCSize files_quota;
-  BCSize folder_quota;
-  BCFolder *root_folder;
-} BCUser;
-
-
-/*
-  Publisher structures and functions
-*/
-
-typedef struct BCPublisher {
-  BCKey public_key;
-  BCTime creation_date;
-  BCKey proof_of_creation;
-  char *nickname;
-  Bool public_metadata;
-  Bool public_files;
-  Bool trust_all_users;
-  BCUser *users;
-  BCInteger n_users;
-} BCPublisher;
-
-
-BCError bc_register_publisher (BCPublisher *publisher);
-BCError bc_get_publisher (BCPublisher *dest, BCKey public_key);
-BCError bc_update_publisher (BCPublisher *publisher);
-
-BCError bc_check_publisher_ca (BCPublisher *publisher);
-BCError bc_check_publisher_creation (BCPublisher *publisher);
-
-BCError bc_get_users (BCUser **dest, BCPublisher *publisher);
-BCError bc_get_user (BCUser *dest, BCKey user_key);
